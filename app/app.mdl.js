205bd5d89bfa6f595076c8c1106dd3a0a53d8449
Paul <Paul.kinross.gray@gmail.com>
2014-04-10 12:58:14 +0100

    Getting the site ready

---
+++ app/app.mdl.js
@@ -0,0 +1,46 @@
+﻿angular.module('test', ['ngQueryString'])
+.controller('TestCtrl', ['$scope', 'ngQueryString.ngQueryString', function ($scope, ngQueryString) {
+
+    $scope.demo = {};
+
+    $scope.setQueryString = {}
+
+    $scope.setQueryStringClick = function () {
+
+        ngQueryString.setQueryString($scope.setQueryString.name, $scope.setQueryString.value);
+
+    }
+
+    $scope.getQueryString = {};
+
+    $scope.getQueryStringClick = function () {
+
+        $scope.getQueryString.submitted = true;
+        $scope.getQueryString.results = ngQueryString.getQueryString($scope.getQueryString.name);
+
+
+    };
+
+    $scope.getAllQueryString = {};
+
+    $scope.getAllQueryStringClick = function () {
+        $scope.getAllQueryString.submitted = true;
+        $scope.getAllQueryString.results = ngQueryString.getAllQueryString();
+    }
+
+    $scope.clearQueryString = {};
+
+    $scope.clearQueryStringClick = function () {
+         
+        ngQueryString.clearQueryString($scope.clearQueryString.name);
+
+    };
+
+
+    $scope.clearAllQueryStringClick = function () {
+
+        ngQueryString.clearAllQueryString();
+
+    };
+
+}])
